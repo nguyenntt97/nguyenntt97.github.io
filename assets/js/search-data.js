85d4568ffa3dfbb54ef117143341f01eebdd94dd
nguyenntt97 <rohirrim3105@gmail.com>
2025-04-07 15:46:26 +0000

    Deploying to gh-pages from @ nguyenntt97/nguyenntt97.github.io@475cce25b2c770b110ce6666faa462215def98ff 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -429,12 +429,7 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "News",},{id: "projects-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
           section: "Projects",handler: () => {
@@ -474,6 +469,11 @@ ninja.data = [{
           description: "another project with an image 🎉",
           section: "Projects",handler: () => {
               window.location.href = "/projects/9_project/";
+            },},{id: "projects-when-less-is-more-a-sparse-facial-motion-structure-for-listening-motion-learning",
+          title: 'When Less Is More: A Sparse Facial Motion Structure For Listening Motion Learning...',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/sfms_25/";
             },},{
         id: 'social-email',
         title: 'email',
